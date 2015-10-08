6ca504a6e25d104a41076cefc0fac2ab57ba1325
Carles Vallve <snaptothegrid@gmail.com>
2015-10-08 17:24:56 +0900

    Implemented basic game camera movement

---
+++ Assets/Scripts/Camera/GameCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+	private float speed = 0.2f;
+	private float friction = 0.9f;
+
+	private Vector3 movement;
+
+
+	void Update () {
+		if (Input.GetKey(KeyCode.A)) {
+			movement = new Vector3(-speed, movement.y, movement.z);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			movement = new Vector3(speed, movement.y, movement.z);
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			movement = new Vector3(movement.x, movement.y, speed);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			movement = new Vector3(movement.x, movement.y, -speed);
+		}
+
+		movement *= friction;
+
+		transform.Translate(movement, Space.World);
+	}
+}
