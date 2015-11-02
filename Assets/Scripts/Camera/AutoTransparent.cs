a92a95bf6401b10c8a2be58b24579f25acba7e45
Carles Vallve <cpujol@wizcorp.jp>
2015-11-02 18:45:06 +0900

    Got a nice transparent fade in/out effect by not reseting the shader when done

--- Assets/Scripts/Camera/AutoTransparent.cs
+++ Assets/Scripts/Camera/AutoTransparent.cs
@@ -2,45 +2,41 @@
 using System.Collections;
 
 
-public class AutoTransparent : MonoBehaviour
-{
-    private Shader m_OldShader = null;
-    private Color m_OldColor = Color.black;
-    private float m_Transparency = 0.1f;
-    private const float m_TargetTransparancy = 0.1f;
-    private const float m_FallOff = 1f; // returns to 100% in m_FallOff seconds
+public class AutoTransparent : MonoBehaviour {
+	private Shader m_OldShader = null;
+	private Color m_OldColor = Color.black;
+	private float m_Transparency = 1f;// 0.1f
+	private const float m_TargetTransparency = 0.15f;
+	private const float m_FallOff = 0.5f; // returns to 100% in m_FallOff seconds
 
 
- public void BeTransparent()
- {
-     // reset the transparency;
-     m_Transparency = m_TargetTransparancy;
-     if (m_OldShader == null)
-     {
-         // Save the current shader
-         m_OldShader = GetComponent<Renderer>().material.shader;
-         m_OldColor  = GetComponent<Renderer>().material.color;
-         GetComponent<Renderer>().material.shader = Shader.Find("Transparent/Diffuse");
-         //GetComponent<Renderer>().material.shader = Shader.Find("Custom/TransparentDiffuseWithShadow");
-     }
- }
- void Update()
- {
-     if (m_Transparency < 1.0f)
-     {
-         Color C = GetComponent<Renderer>().material.color;
-         C.a = m_Transparency;
-         GetComponent<Renderer>().material.color = C;
-     }
-     else
-     {
-         // Reset the shader
-         GetComponent<Renderer>().material.shader = m_OldShader;
-         GetComponent<Renderer>().material.color = m_OldColor;
-         // And remove this script
-         Destroy(this, m_FallOff);
-     }
-     m_Transparency += ((1.0f-m_TargetTransparancy)*Time.deltaTime) / m_FallOff;
- }
+	public void BeTransparent() {
+		// reset the transparency;
+		m_Transparency = m_TargetTransparency;
+		if (m_OldShader == null) {
+			// Save the current shader
+			m_OldShader = GetComponent<Renderer>().material.shader;
+			m_OldColor  = GetComponent<Renderer>().material.color;
+			//GetComponent<Renderer>().material.shader = Shader.Find("Transparent/Diffuse");
+			GetComponent<Renderer>().material.shader = Shader.Find("Custom/TransparentDiffuseWithShadow");
+		}
+	}
+
+
+	void Update() {
+		if (m_Transparency < 1.0f) {
+			Color C = GetComponent<Renderer>().material.color;
+			C.a = m_Transparency;
+			GetComponent<Renderer>().material.color = C;
+		} else {
+			// Reset the shader
+			//GetComponent<Renderer>().material.shader = m_OldShader;
+			//GetComponent<Renderer>().material.color = m_OldColor;
+			// And remove this script
+			//Destroy(this, m_FallOff);
+		}
+		
+		m_Transparency += ((1.0f-m_TargetTransparency)*Time.deltaTime) / m_FallOff;
+	}
 
 }
--- Assets/Scripts/Camera/ClearSight.cs
+++ Assets/Scripts/Camera/ClearSight.cs
@@ -6,8 +6,8 @@ public class ClearSight : MonoBehaviour {
 	public LayerMask layerMask;
 
 
-	void Start () {
-		/*Transform mapContainer = GameObject.Find("3ds file").transform;
+	/*void Start () {
+		Transform mapContainer = GameObject.Find("3ds file").transform;
 		Renderer[] renderers = mapContainer.GetComponentsInChildren<Renderer>();
 		
 		foreach (Renderer renderer in renderers) {
@@ -16,26 +16,27 @@ public class ClearSight : MonoBehaviour {
 				Material material = materialsList[i];
 				material.shader = Shader.Find("Custom/Overdraw");
 			}
-		}*/
-	}
+		}
+	}*/
 
 	void Update() {
 
-		Transform mapContainer = GameObject.Find("3ds file").transform;
+		/*Transform mapContainer = GameObject.Find("3ds file").transform;
 		Renderer[] renderers = mapContainer.GetComponentsInChildren<Renderer>();
 		foreach (Renderer renderer in renderers) {
 			renderer.enabled = true;
-		}
+		}*/
 
 		Player player = Game.instance.currentSquad.currentPlayer;
 		Vector3 startPos = Camera.main.transform.localPosition;
-		Vector3 endPos = player.transform.localPosition + Vector3.up * 0.5f;
-		Vector3 dir =  endPos - startPos;
+		Vector3 endPos = player.transform.localPosition + Vector3.up * 1f;
+		Vector3 dir =  (endPos - startPos).normalized;
+        float dist = Vector3.Distance(endPos, startPos);
 
 		RaycastHit[] hits;
 		// you can also use CapsuleCastAll()
 		// TODO: setup your layermask it improve performance and filter your hits.
-		hits = Physics.RaycastAll(transform.position, dir.normalized, dir.magnitude, layerMask);
+		hits = Physics.RaycastAll(startPos, dir, dist, layerMask);
 		foreach(RaycastHit hit in hits) {
 			//if (hit.transform == null) { continue; }
 
@@ -58,10 +59,49 @@ public class ClearSight : MonoBehaviour {
 			 }
 
 			 AT.BeTransparent(); // get called every frame to reset the falloff
-
 		 }
 	 }
 
+
+ 
+    /*public Transform WatchTarget;
+    public LayerMask OccluderMask;
+    //This is the material with the Transparent/Diffuse With Shadow shader
+    //public Material HiderMaterial;
+ 
+    private Dictionary<Transform, Material> _LastTransforms;
+ 
+    void Start () {
+        _LastTransforms = new Dictionary<Transform, Material>();
+    }
+ 
+    void Update () {
+        //reset and clear all the previous objects
+        if(_LastTransforms.Count > 0){
+            foreach(Transform t in _LastTransforms.Keys){
+                t.renderer.material = _LastTransforms[t];
+            }
+            _LastTransforms.Clear();
+        }
+ 
+        //Cast a ray from this object's transform the the watch target's transform.
+        RaycastHit[] hits = Physics.RaycastAll(
+            transform.position,
+            WatchTarget.transform.position - transform.position,
+            Vector3.Distance(WatchTarget.transform.position, transform.position),
+            OccluderMask
+        );
+ 
+        //Loop through all overlapping objects and disable their mesh renderer
+        if(hits.Length > 0){
+            foreach(RaycastHit hit in hits){
+                if(hit.collider.gameObject.transform != WatchTarget && hit.collider.transform.root != WatchTarget){
+                    _LastTransforms.Add(hit.collider.gameObject.transform, hit.collider.gameObject.renderer.material);
+                    hit.collider.gameObject.renderer.material = HiderMaterial;
+                }
+            }
+        }
+    }*/
 
 	 
  }
